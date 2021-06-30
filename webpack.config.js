957ef13887b6e4eca6727c0cdcb74e85abc8b25c
pedrohb29 <pedrobotecchi@gmail.com>
2021-06-29 22:17:18 -0300

    first commit, initial project structure

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const path = require('path');
+
+module.exports = {
+  entry: "./app/index.js",
+  output: {
+      path: path.resolve(__dirname, 'build'),
+      publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader'
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './dist'
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: path.resolve(__dirname,"app", "index.html"),
+      filename: 'index.html'
+    })
+  ]
+}
